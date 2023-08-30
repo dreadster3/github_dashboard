b4f2dc0ebf57ce661cb42d3c6ef35c9431b22669
dreadster3 <afonso.antunes@live.com.pt>
2023-08-30 19:22:33 +0100

    Fix layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,8 +3,8 @@ import SideNav from './SideNav';
 
 function Layout() {
     return (
-        <div className="flex-1 min-h-screen bg-gray-200">
-            <div className="flex flex-row">
+        <div className="flex min-h-screen bg-gray-200">
+            <div className="flex flex-row w-full">
                 <SideNav />
                 <div className="pt-10 w-full h-full">
                     <Outlet />
