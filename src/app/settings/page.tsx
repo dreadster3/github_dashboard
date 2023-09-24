61e7db546e68ec438c5be9bed377b22f11c072cf
dreadster3 <afonso.antunes@live.com.pt>
2023-09-24 23:41:34 +0100

    Update example env and side navigation for settings page

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,11 +1,20 @@
 'use client';
 
 import Title from '@/components/Title';
+import { useSideNavigation } from '@/providers/SideNavProvider';
+import { useEffect } from 'react';
 
 function Page() {
+    const { set_menu_items } = useSideNavigation();
+
+    useEffect(() => {
+        set_menu_items(<></>);
+    }, [set_menu_items]);
+
     return (
         <div className="w-full">
             <Title>Settings</Title>
+            <div>Content</div>
         </div>
     );
 }
