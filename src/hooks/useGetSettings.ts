e32699bdefed8d3b7666f0da4911a27284de505c
Nuno Antunes <afonso.antunes@live.com.pt>
2023-10-13 20:43:08 +0100

    Reuse function (#26)

--- src/hooks/useGetSettings.ts
+++ src/hooks/useGetSettings.ts
@@ -1,5 +1,5 @@
 import { auth_options } from '@/app/api/auth/[...nextauth]/route';
-import prisma from '@/lib/prisma';
+import { get_settings_by_user_async } from '@/db/settings';
 import get_query_client from '@/utils/query_client';
 import { useQuery } from '@tanstack/react-query';
 import { getServerSession } from 'next-auth';
@@ -20,11 +20,7 @@ export const server_prefetch_settings_async = async () => {
     const query_client = get_query_client();
 
     await query_client.prefetchQuery(['settings'], () =>
-        prisma.settings.findUnique({
-            where: {
-                userId: session!.user.id,
-            },
-        }),
+        get_settings_by_user_async(session!.user.id),
     );
 
     return query_client;
